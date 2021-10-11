808a0c6fa0b4a2d5f92afa5f1b74439f9ec26799
Jesús Cervantes Mac <jesus.cervantes081@gmail.com>
2021-10-11 11:35:28 -0500

    added tailwind and started the crud

---
+++ db/schema.rb
@@ -0,0 +1,13 @@
+ActiveRecord::Schema.define(version: 2021_10_11_020351) do
+
+  create_table "friends", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "twitter"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+end
